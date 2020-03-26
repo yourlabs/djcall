0964283bcac63f2517c516b00cd11cfe8f615c1c
jpic <jpic@yourlabs.org>
2020-03-26 03:18:15 +0100

    Add admin interface

---
+++ djcall/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+
+from .models import Caller, Call, Cron
+
+
+class MetadataModelAdmin:
+    list_display = [
+        'status',
+        'spooled',
+        'started',
+        'ended',
+        'created',
+    ]
+
+
+@admin.register(Caller)
+class CallerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'callback',
+        'spooler',
+    ] + MetadataModelAdmin.list_display
+
+    list_filter = [
+        'status',
+        'spooler',
+        'priority',
+        'callback',
+    ]
+
+
+@admin.register(Call)
+class CallAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'caller',
+    ] + MetadataModelAdmin.list_display
+
+    list_filter = [
+        'status',
+        'caller__callback',
+    ]
+
+    search_fields = [
+        'caller__callback',
+    ]
+
+
+@admin.register(Cron)
+class CronAdmin(admin.ModelAdmin):
+    list_display = [
+        'minute',
+        'hour',
+        'day',
+        'month',
+        'weekday',
+        'caller',
+    ]
+    readonly_fields = [
+        'caller',
+        'minute',
+        'hour',
+        'day',
+        'month',
+        'weekday',
+    ]
